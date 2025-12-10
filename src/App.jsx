251fb8788a78123db7f7b8f30b18ddd2a0c03e97
munachieze904-max <munachieze904@gmail.com>
2025-12-10 14:33:39 +0100

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+    const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Send the message to your email address
+    const data = {
+      name: name,
+      email: email,
+      message: message
+    };
+    console.log(data); // You can replace this with code to send the data to your email
+  }
+
   return (
-    <div>
-        <h1 className='text-3xl text-red-700'>Milo</h1>
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Name" value={name} onChange={handleNameChange} />
+      <input type="email" placeholder="Email Address" value={email} onChange={handleEmailChange} />
+      <textarea placeholder="Message" value={message} onChange={handleMessageChange}></textarea>
+      <button type="submit">Send</button>
+    </form>
+  );
 }
 
 export default App
